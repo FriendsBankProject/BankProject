d19b935cd8f2a24857dfa51647ba21ae85203db7
Sifouo <S.m.rezaei81@outlook.com>
2022-06-06 16:11:17 +0430

    Add Delete Button

--- view/HomeForm.Designer.cs
+++ view/HomeForm.Designer.cs
@@ -54,6 +54,7 @@
             this.branch_btn = new System.Windows.Forms.Button();
             this.account_btn = new System.Windows.Forms.Button();
             this.info_lbl = new System.Windows.Forms.Label();
+            this.Delete_btn = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.panel3.SuspendLayout();
@@ -69,77 +70,90 @@
             this.panel1.Controls.Add(this.Accounts_list);
             this.panel1.Controls.Add(this.Customers_list);
             this.panel1.Controls.Add(this.Bankers_list);
-            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Location = new System.Drawing.Point(16, 15);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(832, 366);
+            this.panel1.Size = new System.Drawing.Size(1109, 450);
             this.panel1.TabIndex = 0;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(44, 15);
+            this.label4.Location = new System.Drawing.Point(59, 18);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(55, 13);
+            this.label4.Size = new System.Drawing.Size(72, 17);
             this.label4.TabIndex = 34;
             this.label4.Text = "Branches:";
             // 
             // branches_list
             // 
             this.branches_list.FormattingEnabled = true;
-            this.branches_list.Location = new System.Drawing.Point(18, 52);
+            this.branches_list.ItemHeight = 16;
+            this.branches_list.Location = new System.Drawing.Point(24, 64);
+            this.branches_list.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.branches_list.Name = "branches_list";
-            this.branches_list.Size = new System.Drawing.Size(353, 121);
+            this.branches_list.Size = new System.Drawing.Size(469, 148);
             this.branches_list.TabIndex = 33;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(505, 15);
+            this.label3.Location = new System.Drawing.Point(673, 18);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.Size = new System.Drawing.Size(64, 17);
             this.label3.TabIndex = 32;
             this.label3.Text = "Bankers:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(24, 189);
+            this.label2.Location = new System.Drawing.Point(32, 233);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.Size = new System.Drawing.Size(70, 17);
             this.label2.TabIndex = 31;
             this.label2.Text = "Accounts:";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(476, 189);
+            this.label1.Location = new System.Drawing.Point(635, 233);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.Size = new System.Drawing.Size(79, 17);
             this.label1.TabIndex = 30;
             this.label1.Text = "Customers:";
             // 
             // Accounts_list
             // 
             this.Accounts_list.FormattingEnabled = true;
-            this.Accounts_list.Location = new System.Drawing.Point(18, 215);
+            this.Accounts_list.ItemHeight = 16;
+            this.Accounts_list.Location = new System.Drawing.Point(24, 265);
+            this.Accounts_list.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Accounts_list.Name = "Accounts_list";
-            this.Accounts_list.Size = new System.Drawing.Size(353, 134);
+            this.Accounts_list.Size = new System.Drawing.Size(469, 164);
             this.Accounts_list.TabIndex = 29;
             // 
             // Customers_list
             // 
             this.Customers_list.FormattingEnabled = true;
-            this.Customers_list.Location = new System.Drawing.Point(461, 215);
+            this.Customers_list.ItemHeight = 16;
+            this.Customers_list.Location = new System.Drawing.Point(615, 265);
+            this.Customers_list.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Customers_list.Name = "Customers_list";
-            this.Customers_list.Size = new System.Drawing.Size(321, 134);
+            this.Customers_list.Size = new System.Drawing.Size(427, 164);
             this.Customers_list.TabIndex = 28;
             // 
             // Bankers_list
             // 
             this.Bankers_list.FormattingEnabled = true;
-            this.Bankers_list.Location = new System.Drawing.Point(470, 52);
+            this.Bankers_list.ItemHeight = 16;
+            this.Bankers_list.Location = new System.Drawing.Point(627, 64);
+            this.Bankers_list.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Bankers_list.Name = "Bankers_list";
-            this.Bankers_list.Size = new System.Drawing.Size(312, 121);
+            this.Bankers_list.Size = new System.Drawing.Size(415, 148);
             this.Bankers_list.TabIndex = 27;
             // 
             // panel2
@@ -152,81 +166,95 @@
             this.panel2.Controls.Add(this.transaction_list);
             this.panel2.Controls.Add(this.address_list);
             this.panel2.Controls.Add(this.loan_list);
-            this.panel2.Location = new System.Drawing.Point(12, 397);
+            this.panel2.Location = new System.Drawing.Point(16, 489);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(832, 353);
+            this.panel2.Size = new System.Drawing.Size(1109, 434);
             this.panel2.TabIndex = 1;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(44, 11);
+            this.label5.Location = new System.Drawing.Point(59, 14);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(46, 13);
+            this.label5.Size = new System.Drawing.Size(58, 17);
             this.label5.TabIndex = 64;
             this.label5.Text = "Checks:";
             // 
             // check_list
             // 
             this.check_list.FormattingEnabled = true;
-            this.check_list.Location = new System.Drawing.Point(18, 36);
+            this.check_list.ItemHeight = 16;
+            this.check_list.Location = new System.Drawing.Point(24, 44);
+            this.check_list.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.check_list.Name = "check_list";
-            this.check_list.Size = new System.Drawing.Size(353, 147);
+            this.check_list.Size = new System.Drawing.Size(469, 180);
             this.check_list.TabIndex = 63;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(494, 11);
+            this.label6.Location = new System.Drawing.Point(659, 14);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(39, 13);
+            this.label6.Size = new System.Drawing.Size(51, 17);
             this.label6.TabIndex = 62;
             this.label6.Text = "Loans:";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(44, 189);
+            this.label7.Location = new System.Drawing.Point(59, 233);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(71, 13);
+            this.label7.Size = new System.Drawing.Size(94, 17);
             this.label7.TabIndex = 61;
             this.label7.Text = "Transactions:";
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(499, 189);
+            this.label8.Location = new System.Drawing.Point(665, 233);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(48, 13);
+            this.label8.Size = new System.Drawing.Size(64, 17);
             this.label8.TabIndex = 60;
             this.label8.Text = "Address:";
             // 
             // transaction_list
             // 
             this.transaction_list.FormattingEnabled = true;
-            this.transaction_list.Location = new System.Drawing.Point(18, 214);
+            this.transaction_list.ItemHeight = 16;
+            this.transaction_list.Location = new System.Drawing.Point(24, 263);
+            this.transaction_list.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.transaction_list.Name = "transaction_list";
-            this.transaction_list.Size = new System.Drawing.Size(353, 121);
+            this.transaction_list.Size = new System.Drawing.Size(469, 148);
             this.transaction_list.TabIndex = 59;
             // 
             // address_list
             // 
             this.address_list.FormattingEnabled = true;
-            this.address_list.Location = new System.Drawing.Point(459, 214);
+            this.address_list.ItemHeight = 16;
+            this.address_list.Location = new System.Drawing.Point(612, 263);
+            this.address_list.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.address_list.Name = "address_list";
-            this.address_list.Size = new System.Drawing.Size(354, 121);
+            this.address_list.Size = new System.Drawing.Size(471, 148);
             this.address_list.TabIndex = 58;
             // 
             // loan_list
             // 
             this.loan_list.FormattingEnabled = true;
-            this.loan_list.Location = new System.Drawing.Point(459, 36);
+            this.loan_list.ItemHeight = 16;
+            this.loan_list.Location = new System.Drawing.Point(612, 44);
+            this.loan_list.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.loan_list.Name = "loan_list";
-            this.loan_list.Size = new System.Drawing.Size(354, 147);
+            this.loan_list.Size = new System.Drawing.Size(471, 180);
             this.loan_list.TabIndex = 57;
             // 
             // panel3
             // 
+            this.panel3.Controls.Add(this.Delete_btn);
             this.panel3.Controls.Add(this.transaction_txt);
             this.panel3.Controls.Add(this.loan_btn);
             this.panel3.Controls.Add(this.check_btn);
@@ -234,61 +262,68 @@
             this.panel3.Controls.Add(this.branch_btn);
             this.panel3.Controls.Add(this.account_btn);
             this.panel3.Controls.Add(this.info_lbl);
-            this.panel3.Location = new System.Drawing.Point(12, 788);
+            this.panel3.Location = new System.Drawing.Point(16, 970);
+            this.panel3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(832, 130);
+            this.panel3.Size = new System.Drawing.Size(1109, 160);
             this.panel3.TabIndex = 2;
             // 
             // transaction_txt
             // 
-            this.transaction_txt.Location = new System.Drawing.Point(501, 91);
+            this.transaction_txt.Location = new System.Drawing.Point(668, 112);
+            this.transaction_txt.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.transaction_txt.Name = "transaction_txt";
-            this.transaction_txt.Size = new System.Drawing.Size(140, 23);
+            this.transaction_txt.Size = new System.Drawing.Size(187, 28);
             this.transaction_txt.TabIndex = 63;
             this.transaction_txt.Text = "Add Transaction";
             this.transaction_txt.UseVisualStyleBackColor = true;
             // 
             // loan_btn
             // 
-            this.loan_btn.Location = new System.Drawing.Point(501, 52);
+            this.loan_btn.Location = new System.Drawing.Point(668, 64);
+            this.loan_btn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.loan_btn.Name = "loan_btn";
-            this.loan_btn.Size = new System.Drawing.Size(140, 23);
+            this.loan_btn.Size = new System.Drawing.Size(187, 28);
             this.loan_btn.TabIndex = 62;
             this.loan_btn.Text = "Add Loan";
             this.loan_btn.UseVisualStyleBackColor = true;
             // 
             // check_btn
             // 
-            this.check_btn.Location = new System.Drawing.Point(501, 13);
+            this.check_btn.Location = new System.Drawing.Point(668, 16);
+            this.check_btn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.check_btn.Name = "check_btn";
-            this.check_btn.Size = new System.Drawing.Size(140, 23);
+            this.check_btn.Size = new System.Drawing.Size(187, 28);
             this.check_btn.TabIndex = 61;
             this.check_btn.Text = "Add Check";
             this.check_btn.UseVisualStyleBackColor = true;
             // 
             // banker_btn
             // 
-            this.banker_btn.Location = new System.Drawing.Point(676, 91);
+            this.banker_btn.Location = new System.Drawing.Point(901, 112);
+            this.banker_btn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.banker_btn.Name = "banker_btn";
-            this.banker_btn.Size = new System.Drawing.Size(137, 23);
+            this.banker_btn.Size = new System.Drawing.Size(183, 28);
             this.banker_btn.TabIndex = 60;
             this.banker_btn.Text = "Add Banker";
             this.banker_btn.UseVisualStyleBackColor = true;
             // 
             // branch_btn
             // 
-            this.branch_btn.Location = new System.Drawing.Point(676, 52);
+            this.branch_btn.Location = new System.Drawing.Point(901, 64);
+            this.branch_btn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.branch_btn.Name = "branch_btn";
-            this.branch_btn.Size = new System.Drawing.Size(137, 23);
+            this.branch_btn.Size = new System.Drawing.Size(183, 28);
             this.branch_btn.TabIndex = 59;
             this.branch_btn.Text = "Add Branch";
             this.branch_btn.UseVisualStyleBackColor = true;
             // 
             // account_btn
             // 
-            this.account_btn.Location = new System.Drawing.Point(676, 13);
+            this.account_btn.Location = new System.Drawing.Point(901, 16);
+            this.account_btn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.account_btn.Name = "account_btn";
-            this.account_btn.Size = new System.Drawing.Size(137, 23);
+            this.account_btn.Size = new System.Drawing.Size(183, 28);
             this.account_btn.TabIndex = 58;
             this.account_btn.Text = "Add Account";
             this.account_btn.UseVisualStyleBackColor = true;
@@ -296,20 +331,32 @@
             // info_lbl
             // 
             this.info_lbl.AutoSize = true;
-            this.info_lbl.Location = new System.Drawing.Point(44, 62);
+            this.info_lbl.Location = new System.Drawing.Point(59, 76);
+            this.info_lbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.info_lbl.Name = "info_lbl";
-            this.info_lbl.Size = new System.Drawing.Size(0, 13);
+            this.info_lbl.Size = new System.Drawing.Size(0, 17);
             this.info_lbl.TabIndex = 57;
+            // 
+            // Delete_btn
+            // 
+            this.Delete_btn.Location = new System.Drawing.Point(431, 16);
+            this.Delete_btn.Margin = new System.Windows.Forms.Padding(4);
+            this.Delete_btn.Name = "Delete_btn";
+            this.Delete_btn.Size = new System.Drawing.Size(187, 28);
+            this.Delete_btn.TabIndex = 64;
+            this.Delete_btn.Text = "Delete Selected";
+            this.Delete_btn.UseVisualStyleBackColor = true;
             // 
             // HomeForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoScroll = true;
-            this.ClientSize = new System.Drawing.Size(880, 430);
+            this.ClientSize = new System.Drawing.Size(1173, 529);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "HomeForm";
             this.Text = "HomeForm";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.HomeForm_FormClosed);
@@ -352,5 +399,6 @@
         private System.Windows.Forms.Button branch_btn;
         private System.Windows.Forms.Button account_btn;
         private System.Windows.Forms.Label info_lbl;
+        private System.Windows.Forms.Button Delete_btn;
     }
 }
